c3066b09487bc1c944381f783cfefd0450e55393
kitts <kittsmakokha@gmail.com>
2023-08-18 09:34:31 +0000

    task 1

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from sqlalchemy import Column, Integer, String
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False, unique=True)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String, nullable=True)
+    reset_token = Column(String, nullable=True)
+
+    def __repr__(self):
+        return f"<User(id={self.id},email='{self.email}')>"
